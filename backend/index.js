84c3c14f792e4ef382915b7ada17a239269d0ea1
Neeraj Kumar <nk7075034@gmail.com>
2023-09-23 20:21:30 +0530

    fix: fixed the console log for the backend server when connected

--- backend/index.js
+++ backend/index.js
@@ -11,5 +11,5 @@ connectDB();
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server running at ${PORT}`.yellow.bold);
+    console.log(`Server running at port ${PORT}`.yellow.bold);
 });
