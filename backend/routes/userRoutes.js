dc4985a1df38c189a75c25d8bcff0b621a908fef
Neeraj Kumar <nk7075034@gmail.com>
2023-09-28 16:46:28 +0530

    feat: implemented search user functionality and added an authorization middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,15 @@
 const express = require("express");
-const { registerUser, authUser } = require("../controllers/userController");
+const {
+    registerUser,
+    authUser,
+    allUser,
+} = require("../controllers/userController");
+const { protect } = require("../middleware/authorizationMiddleware");
 
 const router = express.Router();
 
 router.route("/signup").post(registerUser);
 router.route("/login").post(authUser);
+router.route("/").get(protect, allUser);
 
 module.exports = router;
