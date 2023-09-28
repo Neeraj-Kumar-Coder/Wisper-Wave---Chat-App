ecf5a59ef0197823e496621e6b6695d754904a51
Neeraj Kumar <nk7075034@gmail.com>
2023-09-28 11:06:12 +0530

    feat: implemented login functionality

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -6,12 +6,18 @@ import {
     InputGroup,
     InputRightElement,
     VStack,
+    useToast,
 } from "@chakra-ui/react";
 import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
     const [showPassword, setShowPassword] = useState(false);
     const [formData, setFormData] = useState({ email: "", password: "" });
+    const [loading, setLoading] = useState(false);
+    const toast = useToast();
+    const navigate = useNavigate();
 
     const onChangeHandler = (event) => {
         setFormData({
@@ -25,6 +31,59 @@ const Login = () => {
             email: "guest.user@guest.com",
             password: "guestuser12345",
         });
+    };
+
+    const submitHandler = async () => {
+        setLoading(true);
+
+        if (!formData.email || !formData.password) {
+            toast({
+                title: "Enter valid credentials",
+                status: "error",
+                duration: 3000,
+                isClosable: true,
+                position: "top",
+            });
+            setLoading(false);
+            return;
+        }
+
+        const config = {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        };
+
+        try {
+            const { data } = await axios.post(
+                "/api/user/login",
+                formData,
+                config
+            );
+
+            toast({
+                title: "Login Successfull",
+                status: "success",
+                duration: 3000,
+                isClosable: true,
+                position: "top",
+            });
+
+            localStorage.setItem("user-info", JSON.stringify(data));
+            setLoading(false);
+            navigate("/chats");
+        } catch (error) {
+            toast({
+                title: "Error Occured!",
+                description: error.response.data.message,
+                status: "error",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+
+            setLoading(false);
+        }
     };
 
     return (
@@ -59,7 +118,13 @@ const Login = () => {
                     </InputRightElement>
                 </InputGroup>
             </FormControl>
-            <Button variant={"outline"} colorScheme="blue" w={"100%"}>
+            <Button
+                variant={"outline"}
+                colorScheme="blue"
+                w={"100%"}
+                isLoading={loading}
+                onClick={submitHandler}
+            >
                 Login
             </Button>
             <Button
