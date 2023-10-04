cc78c9752e2c892a04039144e66d06662c9b02fc
Neeraj Kumar <nk7075034@gmail.com>
2023-10-04 20:27:53 +0530

    feat: removed unused code [checkpoint]

--- frontend/src/components/Chats.js
+++ frontend/src/components/Chats.js
@@ -1,10 +1,7 @@
 import React from "react";
-import { ChatState } from "../contexts/ChatProvider";
 import NavBar from "./NavBar";
 
 const Chats = () => {
-    const { user } = ChatState();
-
     return (
         <>
             <NavBar />
